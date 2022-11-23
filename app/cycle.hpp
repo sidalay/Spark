7dbec48ee37339c97d16ad8105c9d6b48f5e96b4
Sid Shingaki <sidalaytv@gmail.com>
2022-11-23 11:45:37 -0800

    feat: defined project file structure

---
+++ app/cycle.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <filesystem>
+
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
+#endif
+#include <GLFW/glfw3.h>
+
+void start_cycle();
+
+void end_cycle(GLFWwindow* const window);
---
+++ app/render.cpp
@@ -0,0 +1,8 @@
+#include "render.hpp"
+#include "imgui.h"
+#include "imgui_demo.cpp"
+#include <iostream>
+
+void render() {
+    ImGui::ShowDemoWindow();
+}
